848b9c18b32e2ccf1cc3123aaf7961ef75cc7859
Whyem <yassirmerimi@gmail.com>
2021-08-07 01:38:00 +0100

    components creation + retrieving data from git

---
+++ src/App.vue
@@ -0,0 +1,125 @@
+<template>
+  <Repos v-for="(repo, index) in repos" :key="repo.id" 
+  :img="repo.owner.avatar_url" 
+  :name="repo.name" 
+  :description="repo.description" 
+  :stars="repo.stargazers_count" 
+  :issues="repo.open_issues_count"
+  :username="repo.owner.login"
+  :userlink="repo.owner.url" 
+  :index = "index"/>
+</template>
+
+<script>
+import Repos from './components/Repos.vue'
+import axios from 'axios'
+
+export default {
+  name: 'App',
+  components: {
+    Repos
+  },
+  data () {
+    return {
+      repos: [],    
+    }
+  },
+   methods: {
+        
+        getFormattedDate() {
+          var date = new Date();
+          date.setDate(date.getDate() - 30);
+          let year = date.getFullYear();
+          let month = (1 + date.getMonth()).toString().padStart(2, '0');
+          let day = date.getDate().toString().padStart(2, '0');
+        
+          return year + '-' + month + '-' + day;
+        }
+    },  
+  mounted () {
+    axios
+      .get('https://api.github.com/search/repositories?q=created:%3E'+this.getFormattedDate()+'&sort=stars&order=desc')
+      .then( response => {
+        this.repos = response.data.items;
+        console.log(this.repos)
+      })
+  }
+}
+</script>
+
+<style>
+html {
+  font-size: 100%;
+  box-sizing: border-box;
+}
+
+*, *::after, *::before {
+  box-sizing: border-box;
+}
+
+body {
+  padding: 0;
+  margin: 0;
+  font-size: 12px;
+  font-weight: 400;
+  min-height: 100vh;
+  font-family: 'Spartan', sans-serif;
+  background-image: url("./assets/git.png");
+  background-repeat: space;
+  background-size: 35px;
+  position: relative;
+  line-height: 1;
+  padding: 60px 0 60px 0;
+  text-align: center;
+}
+
+@media (max-width: 768px) {
+  body {
+    padding: 60px 10px 80px 10px;
+  }
+}
+
+@media (min-width: 768px) {
+  .fd-bm {
+    display: flex;
+  }
+}
+
+@media (max-width: 768px) {
+  .fd-bm {
+    display: block;
+  }
+}
+
+.flex {
+  display: flex;
+}
+
+.flex--jc {
+  justify-content: center;
+}
+
+.flex--jcfs {
+  justify-content: flex-start;
+}
+
+.flex--jcfe {
+  justify-content: flex-end;
+}
+
+.flex--jsb {
+  justify-content: space-between;
+}
+
+.flex--jsa {
+  justify-content: space-around;
+}
+
+.flex--ai {
+  align-items: center;
+}
+
+h1, h2, p, small {
+  margin: 0;
+}
+</style>
---
+++ src/components/Repos.vue
@@ -0,0 +1,180 @@
+<template>
+  <div class="repo"  v-bind:class="{ gold: index == 0 , silver: index == 1, bronze: index == 2}">
+            <div class="fd-bm flex--ai">
+                <div class="repo__img">
+                    <img :src="img" class="avatar" alt="avatar">
+                </div>
+    
+                <div class="repo__text">  
+                    <div class="text_block">
+                        <h1 class="repo__title">{{ name }}</h1>
+                        <p class="repo__description">{{ description }}</p>
+                    </div>
+    
+                    <div class="fd-bm flex--ai">
+                        <div class="tags flex flex--ai flex--jc">
+                            <div class="tag flex flex--ai">
+                                <img src="../assets/star.svg" class="tag__icon yellow" alt="star">
+                                <p class="tag__text">Stars</p>
+                                <p class="tag__number yellow">{{ stars }}</p>
+                            </div>
+    
+                            <div class="tag flex flex--ai">
+                                <img src="../assets/issue.svg" class="tag__icon" alt="star">
+                                <p class="tag__text">Issues</p>
+                                <p class="tag__number">{{ issues }}</p>
+                            </div>
+                        </div>
+    
+                        <div class="submitted">
+                            Submitted 30 days ago by <a :href="userlink" class="user_link">{{ username }}</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</template>
+
+<script>
+export default {
+  name: 'Repos',
+  props: {
+    index: Number,
+    img: String,
+    name: String,
+    description: String,
+    stars: String,
+    issues: String,
+    username: String,
+    userlink: String,
+  },
+  data () {
+    return {
+      star: require('../assets/star.svg'),
+      issue: require('../assets/issue.svg')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.repo {
+  background: #F2F2F2;
+  padding: 22px 80px 22px 15px;
+  border-radius: 5px;
+  display: inline-block;
+  box-shadow: 0px 5px 9px #8d8d8ddb;
+  text-align: left;
+  margin-bottom: 26px;
+}
+
+@media (max-width: 768px) {
+  .repo {
+    text-align: center;
+    padding: 22px 50px;
+  }
+}
+
+@media (max-width: 376px) {
+  .repo {
+    text-align: center;
+    padding: 22px 15px;
+  }
+}
+
+.repo__title {
+  text-transform: uppercase;
+  margin-bottom: 8px;
+}
+
+.repo__img {
+  border-radius: 7px;
+  overflow: hidden;
+  margin-right: 36px;
+  width: 105px;
+}
+
+@media (max-width: 768px) {
+  .repo__img {
+    margin: 0 auto;
+    margin-bottom: 26px;
+  }
+}
+
+.repo__description {
+  font-weight: 500;
+  color: #838383;
+}
+
+.text_block {
+  margin-bottom: 30px;
+}
+
+.avatar {
+  width: 100%;
+  border-radius: 7px;
+}
+
+.tag {
+  font-weight: bold;
+  color: white;
+  background: black;
+  padding: 9px 7px;
+  border-radius: 4px;
+}
+
+.tag__icon {
+  width: 12px;
+  margin-right: 5px;
+}
+
+.tag__text {
+  margin-right: 15px;
+}
+
+.tag:first-child {
+  margin-right: 12px;
+}
+
+.tag:last-child {
+  margin-right: 14px;
+}
+
+@media (max-width: 768px) {
+  .tag {
+    margin: 0 auto;
+  }
+}
+
+@media (max-width: 768px) {
+  .tags {
+    margin: 0 auto;
+    margin-bottom: 16px;
+  }
+}
+
+.yellow {
+  color: #FFE532;
+}
+
+.user_link {
+  color: #EF2D2D;
+}
+
+.submitted {
+  font-weight: 400;
+  color: #838383;
+}
+
+.gold{
+  background:#FFE532;
+}
+
+.silver{
+  background: #B8B8B8;
+}
+
+.bronze{
+  background: #D46900;
+}
+</style>
